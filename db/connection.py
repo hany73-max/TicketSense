37add0bab1ea5d4312410f33a475d569cecee8e3
hany73-max <hanyelashry323@gmail.com>
2026-08-13 02:06:17 +0300

    adding the data extraction file and minor edits to the connection.py to add the session factory for when needed

--- db/connection.py
+++ db/connection.py
@@ -1,6 +1,9 @@
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine  
+from sqlalchemy.orm import sessionmaker  
 
 engine = create_engine("postgresql://postgres:postgres@localhost:5432/ticketsense", echo=False)
 
 def get_engine():
     return engine
+
+SessionLocal = sessionmaker(bind=engine)
--- src/data_extracting.py
+++ src/data_extracting.py
@@ -1,6 +1,15 @@
+from sqlalchemy import select
+import pandas as pd
+
 from db.connection import get_engine
+from models.schema import Customers, Tickets
+from pipelines.init_db import init_database
 
 engine = get_engine()
+init_database()
 
 def data_extraction():
-    
+    stmt = select(Customers, Tickets).join(Tickets, Customers.customer_id == Tickets.owner)
+    df = pd.read_sql(stmt, engine)
+
+    return df
